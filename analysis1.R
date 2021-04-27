5452ef59b829d846949cee8ab5d3f72f58b26e26
jayquinn <61223949+jayquinn@users.noreply.github.com>
2021-04-27 18:44:23 +0900

    내일 합시다 다 귀찮다

--- analysis1.R
+++ analysis1.R
@@ -1,5 +1,7 @@
 library("psych")
 library(dplyr)
 dat<-read.csv("C:/git/greenglasses/data120.csv",header = T)
-?alpha
-alpha(dat[1:6],na.rm = T)
+#미싱 데이터는 엑셀 단위에서 처리하였음
+##### 역문항 처리 ####
+alpha(dat[,1:6])
+cor(dat[,1:10])
