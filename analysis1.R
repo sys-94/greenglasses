ff5ee850039ba93997eb53623d21c050f7eb84df
jayquinn <61223949+jayquinn@users.noreply.github.com>
2021-04-27 13:33:25 +0900

    역문항 처리해야함 좆됐음

--- analysis1.R
+++ analysis1.R
@@ -1,6 +1,5 @@
 library("psych")
+library(dplyr)
 dat<-read.csv("C:/git/greenglasses/data120.csv",header = T)
-dat_trait<-dat[,2:32]
-head(dat_trait)
-tail(dat_trait)
-alpha(dat_trait,check.keys=TRUE)
+?alpha
+alpha(dat[1:6],na.rm = T)
