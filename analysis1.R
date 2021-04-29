c4dc0e9e516869fecccf3c900279313aec814ecb
jayquinn <61223949+jayquinn@users.noreply.github.com>
2021-04-29 16:16:08 +0900

    활동수준, 경조 분석 끝

--- analysis1.R
+++ analysis1.R
@@ -2,6 +2,11 @@ library("psych")
 library(dplyr)
 dat<-read.csv("C:/git/greenglasses/data120.csv",header = T)
 #미싱 데이터는 엑셀 단위에서 처리하였음
-##### 역문항 처리 ####
-alpha(dat[,1:6])
-cor(dat[,1:10])
+#### 역문항 처리 ####
+dat<-as_tibble(dat)
+rnumbers<-grep("R",colnames(dat))
+dat[rnumbers]<-6-dat[rnumbers]
+edit(dat)
+#### 알파 분석 ####
+alpha(dat[1:6]) #활동수준
+alpha(dat[c(325,326,328:345,228,229)])#경조 p3R제거
